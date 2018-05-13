1d61f5808cd549558c5b42121a8eb3fdf012b7a7
pestbuns <ant.one.dugast@gmail.com>
2018-05-13 17:37:50 +0200

    add Makefile + switch for poll

--- main.c
+++ main.c
@@ -9,8 +9,6 @@
 
 static int poll_gpio(const char *gpio_name)
 {
-    printf("gpio_name=[%s]\n", gpio_name);
-
     // get gpio file descriptor
     int fd = open(gpio_name, O_RDONLY);
     if (fd == -1) {
@@ -28,21 +26,26 @@ static int poll_gpio(const char *gpio_name)
     char buffer[256] = {0};
     while (1) {
 
-        if (poll(&pfd, 1, -1) == -1)
-            perror("poll");
-
-        // check if event happened
-        if (pfd.revents & POLLPRI) {
-
-            sread = read(fd, buffer, sizeof(buffer));
-            if (sread == -1) {
-                perror("read");
-                return -1;
-            } else {
-                printf("buffer[%s][%ld]\n", buffer, sread);
-                memset(buffer, 0, sizeof(buffer));
-            }
-
+        printf("Waiting for event on gpio [%s].\n", gpio_name);
+        switch (poll(&pfd, 1, -1)) {
+            case 0:
+                fprintf(stderr, "poll call timed out\n");
+                break;
+            case -1:
+                perror("poll");
+                break;
+            default:
+                // check if POLLPRI event happened
+                if (pfd.revents & POLLPRI) {
+                    sread = read(fd, buffer, sizeof(buffer));
+                    if (sread == -1) {
+                        perror("read");
+                        return -1;
+                    } else {
+                        printf("buffer[%s][%ld]\n", buffer, sread);
+                        memset(buffer, 0, sizeof(buffer));
+                    }
+                }
         }
     }
 
